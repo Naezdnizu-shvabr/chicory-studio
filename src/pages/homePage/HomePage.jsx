cf373cd36e06364da4c5119865202d37a08fd2bf
teHox1 <orlik.sasha123@gmail.com>
2023-10-31 14:54:35 +0100

    add prettier cfg

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 import DefaultLayout from "../../layouts/Default/DefaultLayout";
 
 const HomePage = () => {
-    return (
-        <DefaultLayout>
-            HomePage
-        </DefaultLayout>
-    );
+    return <DefaultLayout>HomePage</DefaultLayout>;
 };
 
 export default HomePage;
