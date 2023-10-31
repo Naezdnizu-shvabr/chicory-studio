ccb25c16f5a0d128236b552229c14bd153476e3a
TeDdi-pixel <2003valuyga2003@gmail.com>
2023-10-31 17:51:53 +0200

    card

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 import DefaultLayout from "../../layouts/Default/DefaultLayout";
-import Card from '../../shared/card/Card';
+import Card from "../../shared/card/Card";
 
 const HomePage = () => {
-    return (
-        <DefaultLayout>HomePage</DefaultLayout>
-    );
+    return <DefaultLayout>HomePage</DefaultLayout>;
 };
 
 export default HomePage;
