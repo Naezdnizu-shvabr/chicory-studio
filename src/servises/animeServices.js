a88dfe191487e73668e4fa589033fe0d75bd4b58
TeDdi-pixel <2003valuyga2003@gmail.com>
2023-11-01 22:23:46 +0200

    adaptive added

--- src/servises/animeServices.js
+++ src/servises/animeServices.js
@@ -1,25 +1,20 @@
 const animeServices = {
-    getTopAnime: (topAnime,setHomeImage) => {
-    
-            if (topAnime.data && topAnime.data.length > 0) {
-                setHomeImage(topAnime.data[0].images.webp.large_image_url);
-            }
-        
-            return topAnime;
-        },
-    сropText: (text, maxWords) => {
-        const words = text.split(' ');
-    
-        if (words.length > maxWords) {
-            return words.slice(0, maxWords).join(' ') + '...';
+    getTopAnime: (topAnime, setHomeImage) => {
+        if (topAnime.data && topAnime.data.length > 0) {
+            setHomeImage(topAnime.data[0].images.webp.large_image_url);
         }
-    
+
+        return topAnime;
+    },
+    сropText: (text, maxWords) => {
+        const words = text.split(" ");
+
+        if (words.length > maxWords) {
+            return words.slice(0, maxWords).join(" ") + "...";
+        }
+
         return text;
     },
-    
-}
+};
 
-
-
-export {animeServices};
-
+export { animeServices };
--- src/widgets/movieModal/MovieBackscreen.jsx
+++ src/widgets/movieModal/MovieBackscreen.jsx
@@ -1,31 +1,39 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import DefaultTitle from "../../shared/default/defaultTitle/DefaultTitle";
 import DefaultGreyText from "../../shared/default/defaultGreyText/DefaultGreyText";
 import DefaultText from "../../shared/default/defaultText/DefaultText";
 import { animeServices } from "../../servises/animeServices";
 
-const MovieBackscreen = ({topAnime}) => {
-
-    return (
-        topAnime.data && topAnime.data.length > 0 ? (
-            topAnime.data.slice(0, 1).map(anime => (
-                <div className="movie-backscreen" key={anime.mal_id}>
-                    <DefaultTitle title={anime.title} />
-                    <DefaultGreyText text={anime.aired.string} />
-                    <DefaultText
-                        text={animeServices.сropText(anime.synopsis,35)} 
-                    />
-                    <div className="movie-backscreen__genres">
-                        <DefaultGreyText text={anime.genres[0].name} />
-                        <div className="movie-backscreen__dot"></div>
-                        <DefaultGreyText text={anime.genres[1].name} />
-                        <div className="movie-backscreen__dot"></div>
-                        <DefaultGreyText text={anime.genres[2].name} />
-                    </div>
-                </div>
-            ))
-        ) : (null)
-    );
+const MovieBackscreen = ({ topAnime }) => {
+    const [isFullWidth, setIsFullWidth] = useState(false);
+    const handleWindowSize = () => {
+        setIsFullWidth(window.innerWidth <= 885);
+    };
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSize);
+        handleWindowSize();
+        return () => {
+            window.removeEventListener("resize", handleWindowSize);
+        };
+    }, []);
+    return topAnime.data && topAnime.data.length > 0
+        ? topAnime.data.slice(0, 1).map((anime) => (
+              <div className="movie-backscreen" key={anime.mal_id}>
+                  <DefaultTitle title={anime.title} />
+                  <DefaultGreyText text={anime.aired.string} />
+                  <DefaultText
+                      text={animeServices.сropText(anime.synopsis, isFullWidth ? 25 : 35)}
+                  />
+                  <div className="movie-backscreen__genres">
+                      <DefaultGreyText text={anime.genres[0].name} />
+                      <div className="movie-backscreen__dot"></div>
+                      <DefaultGreyText text={anime.genres[1].name} />
+                      <div className="movie-backscreen__dot"></div>
+                      <DefaultGreyText text={anime.genres[2].name} />
+                  </div>
+              </div>
+          ))
+        : null;
 };
 
 export default MovieBackscreen;
